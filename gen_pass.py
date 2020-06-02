fe45bf491ba79846b882559477a55045157e96fa
mattmcmahon <matt.mcmahon424@gmail.com>
2020-06-01 20:48:45 -0400

    version 2

--- gen_pass.py
+++ gen_pass.py
@@ -1,78 +1,85 @@
+from pandas.io.clipboard import clipboard_set
 import random
 import json
-import sys
-import os
 import time
+import math
 import csv
+import sys
 
-NUM_RUNS = 1000
 
-def writer(two, three):
+total = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
 
-    # print(one, two, three)
+
+def writer(passw, length):
 
     with open('random.csv', 'a', newline='') as f:
         fieldnames = ['password', 'length']
         thewriter = csv.DictWriter(f, fieldnames=fieldnames)
 
-        # thewriter.writeheader()
-        thewriter.writerow({'password' : two, 'length' : three})
+        thewriter.writerow({'password' : passw, 'length' : length})
 
 
+def generate(total_len):
 
-def generate():
-
-    with open("word_list.json") as json_file:
+    with open("sorted.json") as json_file:
         data = json.load(json_file)
+    
+    avg_choices = (total_len - 6) / 2
 
-    first_rand_choice = random.choice(list(data.keys()))
+    rand_bin = random.randint(0,1)
 
-    rand_num = random.choice(range(10000,99999))
-    # print(rand_num)
+    for i in range(29):
+        for k, v in data.items():
+            if (v == i):
+                total[i].append(k)
+    
+    if ((total_len % 2) == 1):
+        # print("avg: {}\tbin: {}\n1st: {}\t\t2nd: {}".format(avg_choices, rand_bin,
+        #                                                 math.ceil(avg_choices), math.floor(avg_choices)))
+        if (rand_bin == 0):
+            first_rand_choice = random.choice(total[math.ceil(avg_choices)])
+            second_rand_choice = random.choice(total[math.floor(avg_choices)])
+        else:
+            first_rand_choice = random.choice(total[math.floor(avg_choices)])
+            second_rand_choice = random.choice(total[math.ceil(avg_choices)])
 
-    second_rand_choice = random.choice(list(data.keys()))
-    if second_rand_choice == first_rand_choice:
-        second_rand_choice = random.choice(list(data.keys()))
+    rand_num = random.choice(range(10000, 99999))
 
-    # print(second_rand_choice)
-
-    #spec
-    specs = ['[', '@', '_', '!', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '\\', '|', '}', '{', '~', ':', ']', '+', '=', '.', ',', '`', ';','\'']
+    specs = ['[', '@', '_', '!', '#', '$', '%', '^', '&', '*', '(',')', '<', '>', '?', '/', '\\', '|', '}', '{', '~', ':', ']', '+', '=', '.', ',', '`', ';','\'']
     rand_spec = random.choice(specs)
 
     #combine
     password = first_rand_choice + str(rand_num) + rand_spec + second_rand_choice
-    # print(password)
 
     return password
 
 
-# def checker(min=15,max=21):
-def checker():
-
-    passw = ''
-    runs = 0
-
-    while True:
-
-        passw = generate()
-        # print("{}, {}".format(passw, len(passw)))
-        writer(passw, len(passw))
-        runs += 1
-        if (runs == NUM_RUNS): break
-
-
 def main():
 
-    # checker(sys.argv[1], sys.argv[2])
+    if (len(sys.argv) <= 1):
+        pass_len = 15
+        print("default password length set: 15")
+    else:
+        pass_len = int(sys.argv[1])
+    
+
     start = time.perf_counter()
+    
+    passw = ''
 
-    checker()
-        # os.system("python3 gen_pass.py {} {}".format(sys.argv[1], sys.argv[2]))
+    passw = generate(pass_len)
+
+    writer(passw, len(passw))
+    
+    clipboard_set(passw)
+
+    print("\npassword: {}\t len: {}".format(passw, len(passw)))
     stop = time.perf_counter()
+    print("\ndone in: {}".format(round(stop - start, 2)))
 
-    print("done in:", round(stop - start, 2))
-    print("threads: {}\nruns per: {}".format(0, NUM_RUNS))
+    print("password saved to clipboard for next 20 seconds... \n")
+    time.sleep(20)
+    clipboard_set("")
 
     
 if __name__ == "__main__":
