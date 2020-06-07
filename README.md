79b52d58b24df4b2dd277357ec560967fff8bdde
mattmcmahon <matt.mcmahon424@gmail.com>
2020-06-07 18:31:34 -0400

    cleanup

--- README.md
+++ README.md
@@ -24,7 +24,8 @@ python3 gen_pass.py {password length}
 1. ~~Argument parsing to replace the current "input"~~
 2. Hashing: storing and checking against to make sure password isn't used more than once
 3. ~~Clean up "word_list.json," not all words (i.e. "a" and "aa") need to be in list~~
-4. "Acceptance" or "Rejection" for password
-5. ~~Save to clipboard (goal will be time-limiting save)~~
-6. Compliance with specified framework (i.e NIST)
-7. Argparsing: print to screen, save to file, capitalization (rand letter or first), time to store on clipboard (or at all), etc. 
+4. ~~Save to clipboard (goal will be time-limiting save)~~
+5. Compliance with specified framework (i.e NIST)
+6. ~Argparsing: print to screen, save to file, capitalization (first or second word)~
+7. Number of digits determined by overall length of password (i.e. 12 char password has between 3 and 5 digits)
+8. Allow for leading zeros in digits
--- gen_pass.py
+++ gen_pass.py
@@ -1,4 +1,6 @@
 from pandas.io.clipboard import clipboard_set
+from io import StringIO
+import concurrent.futures
 import random
 import json
 import time
@@ -8,7 +10,7 @@ import argparse
 
 def writer(passw, length):
 
-    with open(args.write, 'a', newline='') as f:
+    with open(args.file, 'a', newline='') as f:
         fieldnames = ['password', 'length']
         thewriter = csv.DictWriter(f, fieldnames=fieldnames)
 
@@ -20,35 +22,40 @@ def list_by_vals(total_dict, term):
     for item in items:
         if item[1] == term:
             key_list.append(item[0])
-    return key_list
+    return random.choice(key_list)
 
 
 def generate(total_len):
 
+    load_time = time.perf_counter()        
     data = json.load(open("sorted.json"))
-    
+
+    if args.verbose:
+        print(f"load time: {round(time.perf_counter() - load_time, 2)}")
+
     first_rand_choice = ""
     second_rand_choice = ""
     rand_bool = bool(random.getrandbits(1))
 
-    if (args.verbose):
-        print(f"total len: {total_len}\tbool: {rand_bool}")
+    rand_true_range = random.randrange(6, total_len - 2)
+    rand_false_range = random.randrange(7, total_len - 3)
 
     if rand_bool:
-        first_len = random.randrange(random.randint(2,3), total_len - random.randrange(6,11))
+        first_len = random.randrange(random.randint(1,2), total_len - rand_true_range)
     else:
-        first_len = random.randrange(random.randint(1,2), total_len - random.randrange(7,12))
+        first_len = random.randrange(random.randint(2,3), total_len - rand_false_range)
     second_len = total_len - 6 - first_len
-
-    if (args.verbose):
-        print(f"fir len: {first_len}\tsec len: {second_len}")
     
-    first_rand_choice = random.choice(list_by_vals(data, first_len))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        first_rand_choice = executor.submit(list_by_vals, data, first_len)
+        second_rand_choice = executor.submit(list_by_vals, data, second_len)
+
+    first_rand_choice = first_rand_choice.result()
 
     if args.caps == 1:
         first_rand_choice = first_rand_choice[0].upper() + first_rand_choice[1:]
 
-    second_rand_choice = random.choice(list_by_vals(data, second_len))
+    second_rand_choice = second_rand_choice.result()
 
     if args.caps == 2:
         second_rand_choice = second_rand_choice[0].upper() + second_rand_choice[1:]
@@ -56,12 +63,11 @@ def generate(total_len):
     rand_num = random.choice(range(10000, 99999))
 
     specs = ['[', '@', '_', '!', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '\\',
-                                 '|', '}', '{', '~', ':', ']', '+', '=', '.', ',', '`', ';','\'']
+                                '|', '}', '{', '~', ':', ']', '+', '=', '.', ',', '`', ';','\'']
     rand_spec = random.choice(specs)
 
 
     return first_rand_choice + str(rand_num) + rand_spec + second_rand_choice
-
 
 
 def main():
@@ -72,13 +78,19 @@ def main():
 
     passw = generate(args.length)
 
+    if args.verbose:
+        print(f"done in: {round(time.perf_counter() - start, 2)}")
+
+    if args.write:
+        args.noclip = True
+        args.show = False
+
     if args.all:
         writer(passw, len(passw))
         print(f"password:\t{passw}\n")
         args.noclip = False
-        args.verbose = True
-    
-    if args.noclip:
+
+    if args.noclip and not args.write:
         args.show = True
 
     if args.show:
@@ -101,11 +113,8 @@ def main():
         
         print("|\n")
         clipboard_set("")
-    
-    if args.verbose:
-        print(f"done in: {round(time.perf_counter() - start, 2)}")
 
-    if args.write:
+    if args.write or args.file:
             writer(passw, len(passw))
 
     
@@ -118,11 +127,12 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--caps", type=int, choices=(1, 2), help="Capitalize first letter or either first word or "
                                                                                             "second word, word; '-c 2' will be second word's first char")
     parser.add_argument("-s", "--show", action="store_true", help="Print password to terminal")
-    parser.add_argument("-w", "--write", type=str, default="password.csv", help="Write to file 'password.csv' or specify. "
+    parser.add_argument("-f", "--file", type=str, default="password.csv", help="File to write to (default: 'password.csv') or specify."
                                                                                 "Caution: mainly used for testing, not a secure write or store")
     group.add_argument("-a", "--all", action="store_true", help="All: Clipboard, Show, Write, and Verbose are used")
     group.add_argument("-nc", "--noclip", action="store_true", help="Do not copy password to clipboard")
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output for analysis")
+    group.add_argument("-w", "--write", action="store_true", help="Write to file only, no printing or clipboard")
     args = parser.parse_args()
 
     main()
